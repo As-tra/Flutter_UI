f1befc1ca41b659ccae0a5c8fd79367f9c97c93d
Assil <astradhdbjd@gmail.com>
2024-09-13 19:52:18 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) {
+      return const MyApp();
+    }),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
+      debugShowCheckedModeBanner: false,
+
+    );
+  }
+}
